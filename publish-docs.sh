afba37afc2c638d2f1d9a2b82d1f2a2a681799c9
David Hotham <david.hotham@blueyonder.co.uk>
2016-10-16 13:14:37 +0100

    Fix remote URL in docs

--- publish-docs.sh
+++ publish-docs.sh
@@ -4,7 +4,7 @@ openssl aes-256-cbc -K "$encrypted_25079192cbe2_key" -iv "$encrypted_25079192cbe
 chmod u=rw,og= ~/.ssh/publish-key
 echo "Host github.com" >> ~/.ssh/config
 echo "  IdentityFile ~/.ssh/publish-key" >> ~/.ssh/config
-git remote set-url origin git@github.com:dimbleby/rust-c-ares.git
+git remote set-url origin git@github.com:dimbleby/tokio-c-ares.git
 git config user.name "Travis CI"
 echo "<meta http-equiv=refresh content=0;url=c_ares/index.html>" > target/doc/index.html
 ~/.local/bin/ghp-import -n target/doc
