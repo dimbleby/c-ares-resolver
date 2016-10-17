5bbb4a343f8ca888ebcbd8b220d62c1aef7862e5
David Hotham <david.hotham@metaswitch.com>
2016-10-17 11:02:15 +0100

    Flesh out resolver interface

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,3 +1,7 @@
+use std::net::{
+    Ipv4Addr,
+    Ipv6Addr,
+};
 use std::sync::{
     Arc,
     Mutex,
@@ -139,6 +143,61 @@ impl Resolver {
         Ok(resolver)
     }
 
+    /// Set the list of servers to contact, instead of the servers specified
+    /// in resolv.conf or the local named.
+    ///
+    /// String format is `host[:port]`.  IPv6 addresses with ports require
+    /// square brackets eg `[2001:4860:4860::8888]:53`.
+    pub fn set_servers(
+        &mut self,
+        servers: &[&str]) -> Result<&mut Self, c_ares::Error> {
+        try!(self.ares_channel.lock().unwrap().set_servers(servers));
+        Ok(self)
+    }
+
+    /// Set the local IPv4 address from which to make queries.
+    pub fn set_local_ipv4(&mut self, ipv4: &Ipv4Addr) -> &mut Self {
+        self.ares_channel.lock().unwrap().set_local_ipv4(ipv4);
+        self
+    }
+
+    /// Set the local IPv6 address from which to make queries.
+    pub fn set_local_ipv6(&mut self, ipv6: &Ipv6Addr) -> &mut Self {
+        self.ares_channel.lock().unwrap().set_local_ipv6(ipv6);
+        self
+    }
+
+    /// Set the local device from which to make queries.
+    pub fn set_local_device(&mut self, device: &str) -> &mut Self {
+        self.ares_channel.lock().unwrap().set_local_device(device);
+        self
+    }
+
+    /// Look up the A records associated with `name`.
+    pub fn query_a(&self, name: &str)
+        -> futures::BoxFuture<c_ares::AResults, c_ares::Error> {
+        let (c, p) = futures::oneshot();
+        self.ares_channel.lock().unwrap().query_a(name, move |result| {
+            c.complete(result);
+        });
+        p.map_err(|_| c_ares::Error::ECANCELLED)
+            .and_then(futures::done)
+            .boxed()
+    }
+
+    /// Look up the AAAA records associated with `name`.
+    pub fn query_aaaa(&self, name: &str)
+        -> futures::BoxFuture<c_ares::AAAAResults, c_ares::Error> {
+        let (c, p) = futures::oneshot();
+        self.ares_channel.lock().unwrap().query_aaaa(name, move |result| {
+            c.complete(result);
+        });
+        p.map_err(|_| c_ares::Error::ECANCELLED)
+            .and_then(futures::done)
+            .boxed()
+    }
+
+    /// Look up the CNAME records associated with `name`.
     pub fn query_cname(&self, name: &str)
         -> futures::BoxFuture<c_ares::CNameResults, c_ares::Error> {
         let (c, p) = futures::oneshot();
@@ -150,6 +209,7 @@ impl Resolver {
             .boxed()
     }
 
+    /// Look up the MX records associated with `name`.
     pub fn query_mx(&self, name: &str)
         -> futures::BoxFuture<c_ares::MXResults, c_ares::Error> {
         let (c, p) = futures::oneshot();
@@ -161,6 +221,7 @@ impl Resolver {
             .boxed()
     }
 
+    /// Look up the NAPTR records associated with `name`.
     pub fn query_naptr(&self, name: &str)
         -> futures::BoxFuture<c_ares::NAPTRResults, c_ares::Error> {
         let (c, p) = futures::oneshot();
@@ -170,6 +231,71 @@ impl Resolver {
         p.map_err(|_| c_ares::Error::ECANCELLED)
             .and_then(futures::done)
             .boxed()
+    }
+
+    /// Look up the NS records associated with `name`.
+    pub fn query_ns(&self, name: &str)
+        -> futures::BoxFuture<c_ares::NSResults, c_ares::Error> {
+        let (c, p) = futures::oneshot();
+        self.ares_channel.lock().unwrap().query_ns(name, move |result| {
+            c.complete(result);
+        });
+        p.map_err(|_| c_ares::Error::ECANCELLED)
+            .and_then(futures::done)
+            .boxed()
+    }
+
+    /// Look up the PTR records associated with `name`.
+    pub fn query_ptr(&self, name: &str)
+        -> futures::BoxFuture<c_ares::PTRResults, c_ares::Error> {
+        let (c, p) = futures::oneshot();
+        self.ares_channel.lock().unwrap().query_ptr(name, move |result| {
+            c.complete(result);
+        });
+        p.map_err(|_| c_ares::Error::ECANCELLED)
+            .and_then(futures::done)
+            .boxed()
+    }
+
+    /// Look up the SRV records associated with `name`.
+    pub fn query_srv(&self, name: &str)
+        -> futures::BoxFuture<c_ares::SRVResults, c_ares::Error> {
+        let (c, p) = futures::oneshot();
+        self.ares_channel.lock().unwrap().query_srv(name, move |result| {
+            c.complete(result);
+        });
+        p.map_err(|_| c_ares::Error::ECANCELLED)
+            .and_then(futures::done)
+            .boxed()
+    }
+
+    /// Look up the TXT records associated with `name`.
+    pub fn query_txt(&self, name: &str)
+        -> futures::BoxFuture<c_ares::TXTResults, c_ares::Error> {
+        let (c, p) = futures::oneshot();
+        self.ares_channel.lock().unwrap().query_txt(name, move |result| {
+            c.complete(result);
+        });
+        p.map_err(|_| c_ares::Error::ECANCELLED)
+            .and_then(futures::done)
+            .boxed()
+    }
+
+    /// Look up the SOA records associated with `name`.
+    pub fn query_soa(&self, name: &str)
+        -> futures::BoxFuture<c_ares::SOAResult, c_ares::Error> {
+        let (c, p) = futures::oneshot();
+        self.ares_channel.lock().unwrap().query_soa(name, move |result| {
+            c.complete(result);
+        });
+        p.map_err(|_| c_ares::Error::ECANCELLED)
+            .and_then(futures::done)
+            .boxed()
+    }
+
+    /// Cancel all requests made on this `Channel`.
+    pub fn cancel(&mut self) {
+        self.ares_channel.lock().unwrap().cancel();
     }
 }
 
