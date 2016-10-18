953cac4db0113e92d348eac56a3df003e533f29d
David Hotham <david.hotham@metaswitch.com>
2016-10-18 22:43:21 +0100

    Only generate docs if we're going to publish them

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+cargo doc -p c-ares
+echo "<meta http-equiv=refresh content=0;url=c_ares_resolver/index.html>" > target/doc/index.html
+
+git config user.name "Travis CI"
+~/.local/bin/ghp-import -n target/doc
+
 openssl aes-256-cbc -K "$encrypted_25079192cbe2_key" -iv "$encrypted_25079192cbe2_iv" -in publish-key.enc -out ~/.ssh/publish-key -d
 chmod u=rw,og= ~/.ssh/publish-key
 echo "Host github.com" >> ~/.ssh/config
 echo "  IdentityFile ~/.ssh/publish-key" >> ~/.ssh/config
 git remote set-url origin git@github.com:dimbleby/c-ares-resolver.git
-git config user.name "Travis CI"
-echo "<meta http-equiv=refresh content=0;url=c_ares_resolver/index.html>" > target/doc/index.html
-~/.local/bin/ghp-import -n target/doc
 git push origin +gh-pages
