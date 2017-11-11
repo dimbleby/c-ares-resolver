c3150fefee09bdd00ccb984ac1e77736755c5c6f
David Hotham <david.hotham@blueyonder.co.uk>
2017-11-11 11:07:00 +0000

    Keep the underlying Resolver alive while futures are unresolved

--- examples/futures.rs
+++ examples/futures.rs
@@ -1,11 +1,13 @@
 // This example demonstrates use of the `FutureResolver`.
 extern crate c_ares;
 extern crate c_ares_resolver;
+extern crate futures;
 extern crate tokio_core;
 
 use std::error::Error;
 
 use c_ares_resolver::FutureResolver;
+use futures::future::Future;
 
 fn print_mx_results(result: &Result<c_ares::MXResults, c_ares::Error>) {
     match *result {
@@ -27,12 +29,16 @@ fn print_mx_results(result: &Result<c_ares::MXResults, c_ares::Error>) {
 
 fn main() {
     // Create Resolver and make a query.
-    let resolver = FutureResolver::new().expect("Failed to create resolver");
-    let query = resolver.query_mx("gmail.com");
+    let query = {
+        let resolver = FutureResolver::new().expect("Failed to create resolver");
+        resolver.query_mx("gmail.com").then(|result| {
+            print_mx_results(&result);
+            result
+        })
+    };
 
     // Run the query to completion and print the results.
     let mut event_loop = tokio_core::reactor::Core::new()
         .expect("Failed to create event loop");
-    let result = event_loop.run(query);
-    print_mx_results(&result);
+    event_loop.run(query).ok();
 }
