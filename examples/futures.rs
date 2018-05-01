28473095e8c4f05ceb8ded512af1c3c7b884bc27
David Hotham <david.hotham@metaswitch.com>
2018-05-01 14:45:27 +0100

    Tweak the futures example
    Be more explicit about what exactly it is demonstrating

--- examples/futures.rs
+++ examples/futures.rs
@@ -11,17 +11,18 @@ use futures::future::Future;
 
 fn main() {
     // Create Resolver and make a query.
-    let query = {
-        let resolver = FutureResolver::new().expect("Failed to create resolver");
-        resolver
-            .query_mx("gmail.com")
-            .map_err(|e| println!("MX lookup failed with error '{}'", e.description()))
-            .map(|results| {
-                for result in &results {
-                    println!("host {}, priority {}", result.host(), result.priority());
-                }
-            })
-    };
+    let resolver = FutureResolver::new().expect("Failed to create resolver");
+    let query = resolver
+        .query_mx("gmail.com")
+        .map_err(|e| println!("MX lookup failed with error '{}'", e.description()))
+        .map(|results| {
+            for result in &results {
+                println!("host {}, priority {}", result.host(), result.priority());
+            }
+        });
+
+    // NB Dropping a FutureResolver does *not* cause outstanding requests to fail.
+    std::mem::drop(resolver);
 
     // Run the query to completion.
     tokio::run(query);
