3a3a1fdb31fc99f94178de54b70b86fc0af6dd51
David Hotham <david.hotham@metaswitch.com>
2021-10-26 23:36:58 +0100

    modernize extern crate

--- src/lib.rs
+++ src/lib.rs
@@ -43,10 +43,6 @@
 //! Further examples showing how to use the library can be found
 //! [here](https://github.com/dimbleby/c-ares-resolver/tree/master/examples).
 #![deny(missing_docs)]
-extern crate c_ares;
-extern crate crossbeam_channel;
-extern crate futures_channel;
-extern crate polling;
 
 mod blockingresolver;
 mod error;
