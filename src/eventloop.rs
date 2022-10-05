625d44df056438278fea3a6c04119144b5d0f2fe
David Hotham <david.hotham@blueyonder.co.uk>
2022-10-05 10:47:01 +0100

    casts

--- src/eventloop.rs
+++ src/eventloop.rs
@@ -62,7 +62,7 @@ impl EventLoop {
                     }
                 } else {
                     let event = polling::Event {
-                        key: socket as usize,
+                        key: usize::try_from(socket).unwrap(),
                         readable,
                         writable,
                     };
@@ -153,7 +153,7 @@ impl EventLoop {
         // in sockets until told otherwise.
         //
         // So re-assert our interest in this socket.
-        let socket = event.key as c_ares::Socket;
+        let socket = c_ares::Socket::try_from(event.key).unwrap();
         {
             let interests = self.interests.lock().unwrap();
             if let Some(Interest(readable, writable)) = interests.get(&socket) {
