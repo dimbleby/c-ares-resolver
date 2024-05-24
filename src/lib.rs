d01fbc7587ed728ef074d231d92b3c93130c4c61
David Hotham <david.hotham@blueyonder.co.uk>
2024-05-24 19:57:57 +0100

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,22 @@
 //! This crate provides three resolver types - the `Resolver`, the `FutureResolver`, and the
 //! `BlockingResolver`:
 //!
-//! * The `Resolver` is the thinnest wrapper around the underlying `c-ares` library.  It returns
-//! answers via callbacks.  The other resolvers are built on top of this.
+//! - The `Resolver` is the thinnest wrapper around the underlying `c-ares` library.  It returns
+//!   answers via callbacks.  The other resolvers are built on top of this.
 //!
-//! * The `FutureResolver` returns answers as `std::future::Future`s.
+//! - The `FutureResolver` returns answers as `std::future::Future`s.
 //!
-//! * The `BlockingResolver` isn't asynchronous at all - as the name suggests, it blocks until the
-//! lookup completes.
+//! - The `BlockingResolver` isn't asynchronous at all - as the name suggests, it blocks until the
+//!   lookup completes.
 //!
 //! On all resolvers:
 //!
-//! -  methods like `query_xxx` correspond to the `c-ares` function `ares_query`, which "initiates
-//! a single-question DNS query".
+//! - methods like `query_xxx` correspond to the `c-ares` function `ares_query`, which "initiates
+//!   a single-question DNS query".
 //!
-//! -  methods like `search_xxx` correspond to the `c-ares` function `ares_search`, which
-//! "initiates a series of single-question DNS queries ... using the channel's search domains as
-//! well as a host alias file given by the HOSTALIAS environment variable".
+//! - methods like `search_xxx` correspond to the `c-ares` function `ares_search`, which
+//!   "initiates a series of single-question DNS queries ... using the channel's search domains as
+//!   well as a host alias file given by the HOSTALIAS environment variable".
 //!
 //! See [`c-ares` documentation](https://c-ares.org/docs.html) for more details.
 //!
